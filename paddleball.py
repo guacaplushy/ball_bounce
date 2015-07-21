8f9a6b9f8a42903c2ffeeb1284a3653dbfd38343
Alain Chiasson <alain.chiasson@example.com>
2015-07-21 19:35:31 -0300

    Added paddle.

--- paddleball.py
+++ paddleball.py
@@ -47,9 +47,16 @@ class Paddle:
         self.canvas.move( self.id, 200, 300 )
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
 
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords( self.id )
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
 
     def turn_left( self, evt ):
         self.x = -2
